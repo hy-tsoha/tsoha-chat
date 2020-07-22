b68c5f49f4b93f7b9e37522134a5002e912e9d70
Dan Lindholm <main@xoudini.com>
2020-07-22 23:11:08 +0300

    Use psycopg2 wrapper, some formatting

--- users.py
+++ users.py
@@ -1,33 +1,42 @@
 from db import db
 from flask import session
-import werkzeug.security
+from werkzeug.security import check_password_hash, generate_password_hash
 
-def login(username,password):
-	sql = "SELECT password, id FROM users WHERE username=:username"
-	result = db.session.execute(sql, {"username":username})
-	user = result.fetchone()
-	if user == None:
-		return False
-	else:
-		if werkzeug.security.check_password_hash(user[0],password):
-			session["user_id"] = user[1]
-			return True
-		else:
-			return False
+def login(username, password):
+    sql = "SELECT password, id FROM users WHERE username = %s"
+
+    try:
+        with db.session as transaction:
+            transaction.execute(sql, (username,))
+            user = transaction.fetchone()
+    except Exception as e:
+        print(e)
+        return False
+
+    if not (user and check_password_hash(user.password, password)):
+        return False
+
+    session.update(user_id=user.id)
+    return True
+
 
 def logout():
-	del session["user_id"]
+    return session.pop("user_id", None)
 
-def register(username,password):
-	hash_value = werkzeug.security.generate_password_hash(password)
-	try:
-		sql = "INSERT INTO users (username,password) VALUES (:username,:password)"
-		db.session.execute(sql, {"username":username,"password":hash_value})
-		db.session.commit()
-	except:
-		print("Virhe tapahtui")
-		return False
-	return login(username,password)
+
+def register(username, password):
+    hash_value = generate_password_hash(password)
+    sql = "INSERT INTO users (username, password) VALUES (%s, %s)"
+
+    try:
+        with db.session as transaction:
+            transaction.execute(sql, (username, hash_value,))
+    except Exception as e:
+        print(e)
+        return False
+
+    return login(username, password)
+
 
 def user_id():
-	return session.get("user_id",0)
+    return session.get("user_id", None)
